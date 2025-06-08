bd8741307ae4abac6c64c6e1c7a90c23004be4a9
Tony stak <stak7085@gmail.com>
2025-06-08 15:42:32 +0530

    feat: environment vailsation

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,10 @@ export default [
 
         languageOptions: {
             ecmaVersion: 2020,
-            globals: globals.browser,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
             parserOptions: {
                 ecmaVersion: 'latest',
                 ecmaFeatures: { jsx: true },
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
+const validateEnv = (envMode, env) => {
+    const requiredVars = ['PORT', 'VITE_ENV']
+
+    for (const key of requiredVars) {
+        if (!env[key]) {
+            throw new Error(`${key} is missing! Please define it in your .env.${envMode}`)
+        }
+    }
+}
+
+const normalizePath = (port) => {
+    const normalizePort = parseInt(port)
+    if (isNaN(normalizePort)) {
+        throw new Error(`Invalid port value: ${port}`)
+    }
+
+    return normalizePort
+}
+
+export default defineConfig(({ mode }) => {
+    if (mode !== 'development' && mode !== 'producation') {
+        throw new Error(`Invalid mode: ${mode}`)
+    }
+    const envMode = mode
+
+    const env = loadEnv(envMode, process.cwd(), '')
+
+    validateEnv(envMode, env)
+
+    const port = normalizePath(env.PORT)
+
+    const config = {
+        port,
+        open: true,
+    }
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: config,
+        preview: config,
+        build: {
+            minify: true,
+        },
+    }
 })
